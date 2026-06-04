cf4df3bdd4c968033947b64eaae88afc7cb665b1
rotsynomena9@gmail.com <rotsynomena9@gmail.com>
2026-06-04 16:53:58 +0300

    Suppression de Increment dans le code

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import Increment from './components/Increment/Increment'
+import TodoList from './components/TodoList'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [show , setShow] = useState(true)
   return (
-    <>
-      <div id='app'>
-        <Increment text="Rotsy"/>
+    <div className='App'>
+        <TodoList/>
       </div>
-    </>
-  )
+  );
 }
 
 export default App
